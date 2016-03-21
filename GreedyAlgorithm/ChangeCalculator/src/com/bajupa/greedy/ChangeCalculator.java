cc020d8e2bee3713529c93029df6c5783056b516
Peter Bauer <peter.bauer@bajupa.com>
2016-03-21 19:28:26 +0100

    First small test cases

---
+++ GreedyAlgorithm/ChangeCalculator/src/com/bajupa/greedy/ChangeCalculator.java
@@ -0,0 +1,34 @@
+package com.bajupa.greedy;
+
+import java.util.*;
+
+/**
+ * Copyright 2016 by Bajupa.com
+ * Created by peter on 21/03/16.
+ */
+public class ChangeCalculator {
+    private List<Integer> availableCoins;
+
+    public static ChangeCalculator HavingTheFollowingCoins(Integer... availableCoins) {
+        return new ChangeCalculator(Arrays.asList(availableCoins));
+    }
+
+    private ChangeCalculator(List<Integer> listOfPossibleCoins) {
+        this.availableCoins = listOfPossibleCoins;
+    }
+
+    public List<Integer> getChangeList(int amountOfChange) {
+        ListIterator<Integer> i = availableCoins.listIterator(availableCoins.size());
+        List<Integer> returnedCoins = new ArrayList<>();
+
+        int currentCoin = i.previous();
+        while (i.hasPrevious() && currentCoin > amountOfChange)
+            currentCoin = i.previous();
+        while (amountOfChange > 0) {
+            returnedCoins.add(currentCoin);
+            amountOfChange -= currentCoin;
+        }
+        return returnedCoins;
+    }
+
+}
