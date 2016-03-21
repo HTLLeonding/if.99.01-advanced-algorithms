abd69aca41ccdf4fc230ce9903f33bb936c635b2
Peter Bauer <peter.bauer@bajupa.com>
2016-03-21 19:34:45 +0100

    First small test cases

---
+++ GreedyAlgorithm/ChangeCalculator/tests/com/bajupa/greedy/ChangeCalculatorTest.java
@@ -0,0 +1,42 @@
+package com.bajupa.greedy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Copyright 2016 by Bajupa.com
+ * Created by peter on 21/03/16.
+ */
+public class ChangeCalculatorTest {
+    private ChangeCalculator changeCalculator;
+
+    @Before
+    public void setUp() {
+        changeCalculator = ChangeCalculator.HavingTheFollowingCoins(1, 2, 5, 10, 20, 50);
+    }
+
+    @Test
+    public void testConstruction() {
+        assertEquals(0, changeCalculator.getChangeList(0).size());
+    }
+
+    @Test
+    public void testFirstFittingCoin() {
+        assertEquals(Collections.singletonList(1), changeCalculator.getChangeList(1));
+    }
+
+    @Test
+    public void testSecondFittingCoin() {
+        assertEquals(Collections.singletonList(2), changeCalculator.getChangeList((2)));
+    }
+
+    @Test
+    public void testTwoFittingCoins() {
+        assertEquals(Arrays.asList(2, 2), changeCalculator.getChangeList(4));
+    }
+}
